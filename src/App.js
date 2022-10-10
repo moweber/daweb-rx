27ca6a8256d907b7cb1ee235caed2ab6b4073d3b
DESKTOP-NK6EJSL\moweber <moworldenterprises@gmail.com>
2022-10-10 10:35:44 -0500

    Add developers Name to header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">TODO: Add Nav</header>
+      <header className="App-header">Matt Weber</header>
       <main className="fluid-container m-4">
         TODO: Add About, Portfolio, Contact, Resume
       </main>
