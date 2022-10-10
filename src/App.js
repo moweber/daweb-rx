f3135101bbd017081af196c8ed71c142a39a31d4
DESKTOP-NK6EJSL\moweber <moworldenterprises@gmail.com>
2022-10-10 10:33:26 -0500

    quick change

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">TODO: Add Nav</header>
+      <header className="App-header">Matt Weber</header>
       <main className="fluid-container m-4">
-        TODO: Add About, Portfolio, Contact, Resume
+        TODO: Add About, Portfolio, Contact, Resume sections
       </main>
+      TODO: Add Footer
     </div>
   );
 }
