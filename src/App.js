aa7b4ced1ed08a6b19c2c6e2e8b2496d6953028e
DESKTOP-NK6EJSL\moweber <moworldenterprises@gmail.com>
2022-10-11 07:01:53 -0500

    Nav links selection indicated About section created. Nav and About tests completed/passing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Nav";
+import About from "./components/About";
 
 function App() {
+  const [cSection, setCSection] = useState("about");
+
   return (
-    <div className="App">
-      <header className="App-header">Matt Weber</header>
-      <main className="fluid-container m-4">
-        TODO: Add About, Portfolio, Contact, Resume
+    <div className="mApp">
+      <header className="mHeader">
+        <Navbar cSection={cSection} setCSection={setCSection}></Navbar>
+      </header>
+      <main className="mContent fluid-container">
+        <About></About>
       </main>
     </div>
   );
